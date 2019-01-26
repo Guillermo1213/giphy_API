780e9f47945e1ef5028e0808e436e4d00ef80e49
Your Name <you@example.com>
2019-01-25 22:36:01 -0800

    Got js to work for generating gifs, rendering both original and still on demanad. Uploaded background and designed buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,19 @@
 var api_key = "&api_key=rsDTmlzMLP7i5V7yhjaxjEUx54ei5UmU";
-var limit = 5;
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  carData + api_key + "&limit=" + limit + "";
+var limit = 10;
+var queryURL;
 var results;
 var rating;
+var stillGif;
+var originalGif;
+var gifTitle;
 var q;
 var carData;
 var newButton;
+var newGif;
 var form_text;
 var newCars = [];
 var carMakeList = ['Abarth', 'AC', 'ACPropulsion', 'Acura', 'A.D.Tramontana', 'AlfaRomeo', 'Almac', 'AlternativeCars', 'Amuza', 'Anteros', 'Arash', 'Ariel', 'Arrinera', 'ASL', 'Asterio', 'AstonMartin', 'Audi', 'BAC', 'Bajaj', 'BeijingAutomobileWorks', 'Bentley', 'BMW', 'Bolloré', 'Bolwell', 'Brilliance', 'Bristol', 'BRMBuggy', 'Brooke', 'Buddy', 'Bufori', 'Bugatti', 'Buick', 'BYD', '', 'Cadillac', 'Caparo', 'Carbontech', 'Carice', 'Changan', 'Changhe', 'Chery', 'Chevrolet', 'Chevron', 'Citroën', 'Chrysler', 'CommuterCars', 'Connaught', 'Corvette', 'Covini', 'CUPRA', '', 'Dacia', 'Daihatsu', 'Datsun', 'DeLaChapelle', 'DMC', 'Diardi', 'Dodge', 'Donkervoort', 'Dongfeng', 'Donto', 'DSAutomobiles', 'DynastiElectricCarCorp.', 'E-Vade', 'Effedi', 'Egy-TechEngineering', 'ElectricRaceabout', 'Elfin', 'Englon', 'Etox', 'Equus', 'Exagon', 'Faralli&Mazzanti', 'FAW', 'Ferrari', 'Fiat', 'Fisker', 'Foday', 'Force', 'Ford', 'FordAustralia', 'FordGermany', 'Fraser', 'GACGroup', 'Galpin', 'Geely', 'Genesis', 'Gibbs', 'Gillet', 'Ginetta', 'GMC', 'GreatWall/Changcheng', 'GTAMotor', 'Hennessey', 'Hindustan', 'Honda', 'Holden', 'HTTTechnologies', 'Hyundai', 'ICML', 'IFRseeAspid', 'IranKhodro/IKCO', 'Infiniti', 'Isdera', 'Isuzu', 'JAC', 'Jaguar', 'Jeep', 'JensenMotors', 'Jetcar', 'Jonway', 'JOSS', 'Kaipan', 'Kantanka', 'Karma', 'Koenigsegg', 'Korres', 'Kia', 'Kiat', 'KishKhodro', 'KTM', 'Lada', 'Lamborghini', 'Lancia', 'LandRover', 'Landwind', 'Laraki', 'LeBlanc', 'Leitch', 'Leopard', 'Lexus', 'Li-ion', 'Lifan', 'Lightning', 'Lincoln', 'Lister', 'LocalMotors', 'Lobini', 'Lotec', 'LotusCars', 'LucraCars', 'Luxgen', 'Mahindra', 'Marussia', 'MarutiSuzuki', 'Maserati', 'Mastretta', 'Mazda', 'McLaren', 'Mercedes-Benz', 'MG', 'Micro', 'MINI', 'Mitsubishi', 'Mitsuoka', 'Morgan', 'Mullen', 'Mycar', 'MyVi-Perodua', 'Nissan', 'Noble', 'Nota', 'Oldsmobile', 'Opel', 'OptimalEnergy', 'Orca', 'Oltcit', 'Pagani', 'Panhard', 'Panoz', 'Perana', 'Perodua', 'Peugeot', 'P.G.O.', 'Polarsun', 'Plymouth', 'Porsche', 'Praga', 'Proto/Oullim', 'Proton', 'Puritalia', 'Qoros', 'Qvale', 'Radical', 'Reliant', 'Renault', 'Reva', 'Rimac', 'Rinspeed', 'Roding', 'Roewe', 'Rolls-Royce', 'Rossin-Bertin', 'Rossion', 'Rover', 'Saab', 'Saleen', 'SAIC-GM-Wuling', 'SAIPA', 'Saker', 'Samsung', 'San', 'Sbarro', 'ScionDefunct', 'SEAT', 'ShanghaiMaple', 'SIN', 'Škoda', 'Smart', 'SpadaVettureSport', 'Spyker', 'Ssangyong', 'SSCNorthAmerica', 'Street&RacingTechnology', 'Subaru', 'Suzuki', 'Tanom', 'Tata', 'Tatra', 'Tauro', 'TawonCar', 'TDCars', 'Tesla', 'ThaiRung', 'Think', 'Toyota', 'Trekka', 'Trident', 'Triumph', 'Troller', 'Trumpchi', 'Tushek', 'TVR', 'TVS', 'Ultima', 'UMM', 'UEV', 'Uri', 'UAZ', 'VauxhallMotors', 'Vector', 'Vencer', 'Venirauto', 'Venturi', 'VEPR', 'Volkswagen', 'Volvo', 'VinFast', 'WMotors', 'Wallyscar', 'Westfield', 'Wheego', 'Wiesmann', 'Yamaha', 'YES!', 'YouabianPuma', 'yomom', 'Yugo', 'ZastavaAutomobiles', 'ZenderCars', 'ZenosCars', 'Zenvo', 'ZiL', 'ZXAuto', 'Zeta'];
-var cars = ["Subaru", "Porsche", "BMW", "Mercedez", "McClaren", "Dodge", "Ford", "Hyundai", "Lamborghini", "Chevrolet"];
+var cars = ["Subaru", "Porsche", "BMW", "McClaren", "Dodge", "Ford", "Hyundai", "Lamborghini", "Chevrolet"];
 
 $(document).ready(function () {
   loadButtons();
@@ -36,13 +39,9 @@ function createButton() {
     newButton.attr("data-name", form_text);
     newButton.text(form_text);
     $("#button_area").append(newButton);
-
+    loadGifs();
   };
 }
-
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//   person + api_key + "&limit=" + limit + "";
-// $(this).attr("data-");
 
 function loadButtons() {
   for (var i = 0; i < cars.length; i++) {
@@ -55,49 +54,44 @@ function loadButtons() {
   }
 }
 
-
-
 function loadGifs() {
   $('.car_button').on('click', function () {
     carData = ($(this)).data('name');
-   
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + carData + api_key + "&limit=" + limit + "";
     $.ajax({
       url: queryURL,
       method: "GET"
     })
 
-      .then(function() {
+      .then(function (response) {
+        for (var i = 0; i < limit; i++) {
+          gifTitle = response.data[i].title;
+          rating = response.data[i].rating;
+          stillGif = response.data[i].images.original_still.url;
+          originalGif = response.data[i].images.original.url;
+          newGif = $('<img>');
+          newGif.attr('class', 'image');
+          $('#gif_area').prepend(newGif[i]);
+          console.log(rating);
+          newGif.addClass("gif");
+          newGif.attr("src", stillGif);
+          newGif.attr("data-original", originalGif);
+          newGif.attr("data-still", stillGif);
+          newGif.attr("data-render", 'still');
+          newGif.text(rating);
+          $("#gif_area").prepend(newGif);
+        }
 
-        console.log(queryURL);
-        
-        //Find what query URl should be
-        // var results = response.data;
-        //We are looking for q, limit, rating, still url and moving url
-        //store response for later use
-        //
-        // for (var i = 0; i < results.length; i++) {
-        //   var gifDiv = $("<div>");
-
-        //   var rating = results[i].rating;
-
-        //   var p = $("<p>").text("Rating: " + rating);
-
-        //   var personImage = $("<img>");
-        //   personImage.attr("src", results[i].images.fixed_height.url);
-
-        //   gifDiv.prepend(p);
-        //   gifDiv.prepend(personImage);
-
-        //   $("#gifs-appear-here").prepend(gifDiv);
-        // }
+        $(".image").on("click", function () {
+          var render = $(this).attr('data-render');
+          if (render === 'still') {
+            $(this).attr('src', $(this).data('original'));
+            $(this).attr('data-render', 'original');
+          } else if (render === 'original') {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-render', 'still');
+          }
+        });
       });
   });
 }
-
-// function create_gifs(){
-//     //grab data from button selected
-//     // add data to queryurl
-//     //run ajax request
-//     //add gifs to gif_area
-//   }
-
